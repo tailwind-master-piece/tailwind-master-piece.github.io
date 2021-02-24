ab528fd58878c5843a6e0add5805ac63781ade9d
Abdiel Ortega <nathanabdiel1@gmail.com>
2021-02-23 19:42:39 -0600

    The typescript configurations was added

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,48 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const babelLoader = {
+  loader: "babel-loader",
+  options: {
+    cacheDirectory: true,
+    presets: ["react", "env"],
+  },
+};
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
   output: {
     filename: "app.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.tsx(x?)$/,
+        use: [
+          babelLoader,
+          {
+            loader: "ts-loader",
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [babelLoader],
+      },
+    ],
+  },
+  devServer: {
+    port: 8080,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
 };
