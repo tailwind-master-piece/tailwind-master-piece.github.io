33cc28c6fec1101c84f05359e767dee883f34273
Abdiel Ortega <nathanabdiel1@gmail.com>
2021-03-01 12:18:28 -0600

    The component was finished

--- src/components/Img/index.tsx
+++ src/components/Img/index.tsx
@@ -3,11 +3,32 @@ import { FC } from 'react';
 interface imgInterface {
   src: string;
   alt?: string;
+  bordered?: boolean;
+  fluid?: boolean;
+  rounded?: boolean;
+  circular?: boolean;
 }
 
-const Img: FC<imgInterface> = ({ src, alt = 'Description' }) => (
-  <div className="img w-40 h-auto">
-    <img className="w-full" src={src} alt={alt} />
+const Img: FC<imgInterface> = ({
+  src,
+  alt = 'Description',
+  bordered,
+  fluid,
+  rounded,
+  circular,
+}) => (
+  <div
+    className={`img w-40 h-auto ${bordered ? 'border border-gray-400' : ''} ${
+      fluid ? 'w-full' : ''
+    } ${rounded ? 'rounded-md' : ''} ${circular ? 'rounded-full' : ''}`}
+  >
+    <img
+      className={`w-full ${rounded ? 'rounded-md' : ''} ${
+        circular ? 'rounded-full' : ''
+      }`}
+      src={src}
+      alt={alt}
+    />
   </div>
 );
 
