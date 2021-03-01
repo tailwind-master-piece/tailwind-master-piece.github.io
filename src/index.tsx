7f0a7b40ec58936da1e7bcf0ecccd36b285e376a
Abdiel Ortega <nathanabdiel1@gmail.com>
2021-03-01 12:20:46 -0600

    An issue was solved

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,4 @@ import Img from './components/Img';
 
 const root = document.getElementById('root');
 
-render(
-  <Img
-    src="https://images.unsplash.com/photo-1481819613568-3701cbc70156?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80"
-    circular
-  />,
-  root
-);
+render(<h1>Hello world</h1>, root);
