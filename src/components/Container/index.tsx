430c3622ae5824b59c0570125fd7b8cf4e5638bc
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-03-07 12:54:33 -0600

    new file:   src/__tests__/components/Container.espect.js 	new file:   src/components/Container/index.tsx 	modified:   src/components/index.ts 	modified:   src/types/index.ts

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,52 @@
+import React, { ReactElement, useEffect, useState } from 'react';
+import {
+  horizontalPadding,
+  verticalPadding,
+  horizontalMargin,
+  verticalMargin,
+  backgroundColor,
+  textColor,
+} from '../../types';
+
+interface containerInterface {
+  children: React.ReactNode;
+  center: boolean;
+  horizontalPadding: horizontalPadding;
+  verticalPadding: verticalPadding;
+  horizontalMargin: horizontalMargin;
+  verticalMargin: verticalMargin;
+  backgroundColor: backgroundColor;
+  textColor: textColor;
+}
+
+const Container: React.FC<containerInterface> = ({
+  children,
+  center,
+  horizontalPadding,
+  verticalPadding,
+  horizontalMargin,
+  verticalMargin,
+  backgroundColor,
+  textColor,
+}): ReactElement => {
+  const [classes, setClasses] = useState('container');
+
+  useEffect(() => {
+    center && setClasses((prevState) => prevState.concat(' mx-auto'));
+    horizontalPadding &&
+      setClasses((prevState) => prevState.concat(` ${horizontalPadding}`));
+    verticalPadding &&
+      setClasses((prevState) => prevState.concat(` ${verticalPadding}`));
+    horizontalMargin &&
+      setClasses((prevState) => prevState.concat(` ${horizontalMargin}`));
+    verticalMargin &&
+      setClasses((prevState) => prevState.concat(` ${verticalMargin}`));
+    backgroundColor &&
+      setClasses((prevState) => prevState.concat(` ${backgroundColor}`));
+    textColor && setClasses((prevState) => prevState.concat(` ${textColor}`));
+  }, []);
+
+  return <div className={classes}>{children}</div>;
+};
+
+export default Container;
