70a770f862c068495de2272407f81b3ad3e9336f
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-03-07 21:12:23 -0600

    new file:   src/components/Header/index.tsx 	modified:   src/components/index.ts 	modified:   src/types/index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from './Button';
 export { default as Loader } from './Loader';
 export { default as Container } from './Container';
 export { default as Divider } from './Divider';
+export { default as Header } from './Header';
--- src/types/index.ts
+++ src/types/index.ts
@@ -510,3 +510,18 @@ export type divideStyle =
   | 'divide-dotted'
   | 'divide-double'
   | 'divide-none';
+
+export type numberOfColumns =
+  | 'grid-cols-1'
+  | 'grid-cols-2'
+  | 'grid-cols-3'
+  | 'grid-cols-4'
+  | 'grid-cols-5'
+  | 'grid-cols-6'
+  | 'grid-cols-7'
+  | 'grid-cols-8'
+  | 'grid-cols-9'
+  | 'grid-cols-10'
+  | 'grid-cols-11'
+  | 'grid-cols-12'
+  | 'grid-cols-none';
