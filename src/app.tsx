4dda07043733f240d7ac1827e32eed6d13e08d54
Abdiel Ortega <nathanabdiel1@gmail.com>
2021-03-09 02:03:07 -0600

    The component input was built

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,7 @@
-import * as React from 'react';
-import { Loader, Button } from './components';
+import Input from './components/Input';
 
 function App(): JSX.Element {
-  return (
-    <div className="p-4">
-      <Button color="blue" radius="lg">
-        <Loader className="mr-2" color="text-indigo-600" size="text-lg" />
-        Loading...
-      </Button>
-    </div>
-  );
+  return <Input icon="right" name="test" />;
 }
 
 export default App;
