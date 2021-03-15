6a0c48e9176f955d81257958c341597657dba831
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-03-14 19:48:01 -0600

    delete no use variables

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { render } from 'react-dom';
 import './assets/styles.css';
-import App from './app';
 
 const root = document.getElementById('root');
 
-render(<App />, root);
+render(<div />, root);
