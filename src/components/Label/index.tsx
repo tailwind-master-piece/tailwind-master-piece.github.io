682b2091b91776f2054244efc8bdd157a4a46e6d
Abdiel Ortega <nathanabdiel1@gmail.com>
2021-03-17 20:58:11 -0600

    The component was created

--- src/components/Label/index.tsx
+++ src/components/Label/index.tsx
@@ -8,7 +8,7 @@ interface labelInterface {
   image?: boolean;
 }
 
-const Label: FC<labelInterface> = ({ text, image }) => (
+const Label: FC<labelInterface> = ({ text }) => (
   <div className="label inline-flex leading-none text-sm align-baseline bg-gray-300 text-gray-700 px-3 py-2 rounded">
     <GrMail
       className="inline-block align-baseline mr-3"
