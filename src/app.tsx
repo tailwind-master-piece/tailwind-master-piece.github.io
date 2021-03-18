5a802c2570e4a61614475eff6b88e6761014e0d7
Abdiel Ortega <nathanabdiel1@gmail.com>
2021-03-18 11:36:57 -0600

    The component label was created

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,10 @@ import Label from './components/Label';
 function App(): JSX.Element {
   return (
     <div className="p-4">
-      <Label text="28" />
+      <Label
+        text="28"
+        src="https://images.unsplash.com/photo-1616066959553-e13b2cfed557?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+      />
     </div>
   );
 }
--- src/components/Label/index.tsx
+++ src/components/Label/index.tsx
@@ -5,16 +5,32 @@ import { FC } from 'react';
 
 interface labelInterface {
   text?: string;
-  image?: boolean;
+  src?: string;
+  alt?: string;
 }
 
-const Label: FC<labelInterface> = ({ text }) => (
-  <div className="label inline-flex leading-none text-sm align-baseline bg-gray-300 text-gray-700 px-3 py-2 rounded">
-    <GrMail
-      className="inline-block align-baseline mr-3"
-      data-testid="label-normal"
-    />
-    <span className="inline-block align-baseline">{text}</span>
+const Label: FC<labelInterface> = ({ text, src, alt }) => (
+  <div
+    className={`label inline-flex leading-none text-sm align-baseline bg-gray-300 text-gray-700 ${
+      !src ? 'px-3 py-2' : 'items-center'
+    } rounded`}
+  >
+    {!src ? (
+      <GrMail
+        className="inline-block align-baseline mr-3"
+        data-testid="label-normal"
+      />
+    ) : (
+      <img
+        data-testid="label-image"
+        className="inline-block mr-2 w-6 h-6 rounded-l"
+        src={src}
+        alt={alt}
+      />
+    )}
+    <span className={`inline-block align-baseline ${src && 'pr-2'}`}>
+      {text}
+    </span>
   </div>
 );
 
