2c9c1bc08d29bee44ebfccd92d61570c8969894c
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-03-22 20:25:02 -0600

    export default components

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
-import { render } from 'react-dom';
 import './assets/styles.css';
 
-const root = document.getElementById('root');
-
-render(<div />, root);
+export { default as Button } from './components/Button';
+export { default as Loader } from './components/Loader';
+export { default as Container } from './components/Container';
+export { default as Divider } from './components/Divider';
+export { default as Header } from './components/Header';
+export { default as Img } from './components/Img';
+export { default as Input } from './components/Input';
