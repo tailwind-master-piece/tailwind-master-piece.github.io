859db0e08a02a74bc1c3c793e3741a94ff92d765
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-03-24 20:11:41 -0600

    add divider and container

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,6 +3,7 @@ import './styles.css';
 
 import Button from '../components/Button/index';
 import Container from '../components/Container/index';
+import Divider from '../components/Divider/index';
 
 export const Landing: React.FC = (): ReactElement => {
   return (
@@ -16,7 +17,7 @@ export const Landing: React.FC = (): ReactElement => {
           <div className="landing-container__component__container buttons">
             <Button>click me</Button>
             <Button color="bg-blue-800">click me</Button>
-            <Button radius="rounded-full" color="bg-indigo-400">
+            <Button radius="rounded-lg" color="bg-indigo-400">
               click me
             </Button>
             <Button
@@ -30,13 +31,91 @@ export const Landing: React.FC = (): ReactElement => {
         </div>
         <div className="landing-container__component">
           <h2>Container</h2>
-          <div className="landing-container__component__container">
-            <Container textColor="text-blue-700">
-              <div>element</div>
-              <div>element</div>
-              <div>element</div>
-              <div>element</div>
+          <div className="landing-container__component__container container">
+            <Container
+              textColor="text-red-700"
+              backgroundColor="bg-blue-300"
+              horizontalMargin="mx-10"
+              horizontalPadding="px-10"
+            >
+              <div>
+                <div>element</div>
+                <div>element</div>
+                <div>element</div>
+                <div>element</div>
+              </div>
             </Container>
+            <Container
+              textColor="text-blue-700"
+              backgroundColor="bg-red-300"
+              horizontalMargin="mx-10"
+              verticalPadding="py-20"
+              horizontalPadding="px-10"
+            >
+              <div>
+                <div>element</div>
+                <div>element</div>
+                <div>element</div>
+                <div>element</div>
+              </div>
+            </Container>
+            <Container
+              textColor="text-black"
+              backgroundColor="bg-green-300"
+              horizontalMargin="mx-10"
+              verticalPadding="py-2"
+              horizontalPadding="px-10"
+            >
+              <div>
+                <div>element</div>
+                <div>element</div>
+                <div>element</div>
+                <div>element</div>
+              </div>
+            </Container>
+            <Container
+              textColor="text-red-700"
+              backgroundColor="bg-yellow-300"
+              horizontalMargin="mx-10"
+              verticalPadding="py-12"
+              horizontalPadding="px-16"
+            >
+              <div>
+                <div>element</div>
+                <div>element</div>
+                <div>element</div>
+                <div>element</div>
+              </div>
+            </Container>
+          </div>
+        </div>
+        <div className="landing-container__component">
+          <h2>Divider</h2>
+          <div className="landing-container__component__container">
+            <div>element</div>
+            <Divider
+              height="divide-y-8"
+              style="divide-dotted"
+              color="divide-purple-700"
+            />
+            <div>element</div>
+            <Divider
+              height="divide-y-4"
+              style="divide-double"
+              color="divide-pink-800"
+            />
+            <div>element</div>
+            <Divider
+              height="divide-y-2"
+              style="divide-dashed"
+              color="divide-green-700"
+            />
+            <div>element</div>
+            <Divider
+              height="divide-y-2"
+              style="divide-solid"
+              color="divide-purple-700"
+            />
           </div>
         </div>
       </div>
